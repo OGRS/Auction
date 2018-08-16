d669568af0d3270af88d3163e0b49e4b83738af8
OGRS <rabotnikasy@gmail.com>
2018-08-16 20:02:07 +0300

    [Task-51] Add form for adding a lot to the auction

--- angular-auction/src/app/components/sign-in/sign-in.component.ts
+++ angular-auction/src/app/components/sign-in/sign-in.component.ts
@@ -8,8 +8,10 @@ import { Router } from '@angular/router';
   templateUrl: 'sign-in.component.html',
   styleUrls: ['sign-in.component.css']
 }) export class SignInComponent implements OnInit {
+
   Error: string;
   isLoginError: boolean = false;
+
   constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
